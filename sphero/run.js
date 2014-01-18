8cc95897eb70c78c9dce627f720a09e352b40885
Dave Welch <david@davidwelch.co>
2014-01-17 19:56:52 -0700

    cleanup; gitignores

--- sphero/run.js
+++ sphero/run.js
@@ -1,72 +1,91 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-var util = require('util');
-var Cylon = require('cylon')
-        , MAX_SPEED = 60;
+var util = require('util')
+  , _ = require('lodash')
+  , Cylon = require('cylon')
+  , MAX_SPEED = 60
+  , Firebase = require('firebase')
+  , rootRef = new Firebase('https://tug-a-sphero.firebaseio.com/ballots/-JDX8SoPDXn2bFk9KiRN')
+  , exec = require('child_process').exec;
+  ;
 
-var Firebase = require('firebase');
-var rootRef = new Firebase('https://tug-a-sphero.firebaseio.com/ballots/-JDX8SoPDXn2bFk9KiRN');
 
 
+function roll(data, sphero){
+  var dir, speed,
+    redCount = data['eastCount'],
+    blueCount = data['westCount'],
+    team = data['changed'];
+
+  if(team == "eastCount"){
+    dir = 1;
+    speed = redCount - blueCount;
+    sphero.setColor('red');
+  }else if(team == "westCount"){
+    dir = 181;
+    speed = blueCount - redCount;
+    sphero.setColor('blue');
+  }else{
+    console.log("Unknown team ", team);
+    return;
+  }
+
+  if( redCount == blueCount ){
+    sphero.stop();
+    sphero.setColor('green');
+  }else{
+    // speed = speed * 20;
+    // if (speed > MAX_SPEED ) speed = MAX_SPEED;
+    speed = MAX_SPEED;
+    sphero.roll(speed, dir);
+    console.log("Moving: ", dir);
+  }
+}
 
 
-        
-var util = require('util'),
-    _ = require('lodash');
- 
+function setup(sphero){
+  Cylon.robot({
+    connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/' + sphero},
+    device: {name: 'sphero', driver: 'sphero'},
+    work: function spheroWorker(my){
+      var sphero = my.sphero;
 
-function spheroWorker(my){
-          var s = my.sphero,
-                  redCount = 0, 
-                  blueCount = 0,
-                  team = undefined
-                  ;
-          
+      console.log("Connected...");
+      sphero.setColor('green');
+      sphero.setBackLED(100);
 
-        console.log("Connected...");
-        s.setColor('green');
-        s.setBackLED(100);
+      rootRef.on('value', function(data){
+        console.log(data.val());
+        roll(data.val(), sphero);
+      });
+    }
+  }).start();
+}
 
-        function roll(data){
-          var dir, speed;
-          redCount = data['eastCount'];
-          blueCount = data['westCount'];
-          team = data['changed'];
-          if(team == "eastCount"){
-                  dir = 1;
-                  speed = redCount - blueCount;
-                  s.setColor('red');
-          }else if(team == "westCount"){
-                  dir = 181;
-                  speed = blueCount - redCount;
-                  s.setColor('blue');
-          }else{
-                  console.log("Unknown team ", team);
-                  return;
-          }
 
-          if( redCount == blueCount ){
-                  s.stop();
-                  s.setColor('green');
-                  return;
-          }
+function selectSphero(options){
+  var result;
+  switch(options.length){
+    case 0:
+      console.log("No sign of a sphero. Have you paired it?");
+      return;
 
-          // speed = speed * 20;
-          // if (speed > MAX_SPEED ) speed = MAX_SPEED;
-          speed = 60;
+    case 1:
+      console.log('Automatically selected ', options[0]);
+      result = options[0];
+      break;
 
-          s.roll(speed, dir);
-          console.log("Moving: ", dir);
-        }
+    default:
+      console.log('You had', options.length, 'options... maybe we might give you a menu to select from eventually?');
+      // TODO give em an option...
+      return;
+  }
 
-        rootRef.on('value', function(data){
-          console.log(data.val());
-          roll(data.val());
-        });
-};
+  setup(result);
+}
 
-Cylon.robot({
-        connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-OYR-AMP-SPP' },
-        device: {name: 'sphero', driver: 'sphero'},
-        work: spheroWorker
-}).start();
+exec('ls /dev | grep tty.Sph', function(err, result, stderr){ 
+  selectSphero(result.trim().split('\n'));
+});
+
+
